81a84ea96b76897dede8823ad498960bb845bffa
Szpedy <romaniak7@gmail.com>
2019-08-19 21:41:13 +0200

    Finished PigLatinTranslator

--- pig-latin/src/main/java/PigLatinTranslator.java
+++ pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PigLatinTranslator {
+
+    private final static String[] vowels = {"A", "E", "I", "O", "U",};
+    private final static String[] doubleVowels = {"YT", "XR"};
+    private final static String[] consontant = {"XR", "YT", "QU"};
+
+    public String translate(String phrase) {
+        return Stream
+                .of(phrase.split(" "))
+                .filter(word -> word.length() >= 2)
+                .map(x -> wordToPig(x, 0))
+                .collect(Collectors.joining(" "));
+    }
+
+    private String wordToPig(String word, int position) {
+        if (Arrays.asList(vowels).contains(word.substring(0, 1).toUpperCase())
+                | Arrays.asList(doubleVowels).contains(word.substring(0, 2).toUpperCase())
+                | position >= word.length() - 1) {
+            return word + "ay";
+        } else if (Arrays.asList(consontant).contains(word.substring(0, 2).toUpperCase())) {
+            return wordToPig(word.substring(2) + word.substring(0, 2), position + 1);
+        } else
+            return wordToPig(word.substring(1) + word.substring(0, 1), position + 1);
+    }
+
+
 }
