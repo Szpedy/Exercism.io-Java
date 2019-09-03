87c5102b258ac8693d6fbd3771087f56a1309c70
Szpedy <romaniak7@gmail.com>
2019-09-03 18:06:53 +0200

    First stage of Bob

--- bob/src/main/java/Bob.java
+++ bob/src/main/java/Bob.java
@@ -1,3 +1,15 @@
-public class Bob {
+import java.util.regex.Pattern;
 
+public class Bob {
+    Pattern question = Pattern.compile("^[\\w,]\\?$");
+    Pattern yell = Pattern.compile("^[A-Z|0-9 ,]+!?$");
+    public String hey(String phrase) {
+        if (question.matcher(phrase).matches())
+            return "Sure.";
+        else if(yell.matcher(phrase).matches())
+            return "Whoa, chill out!";
+        else{
+            return "Whatever.";
+        }
+    }
 }
